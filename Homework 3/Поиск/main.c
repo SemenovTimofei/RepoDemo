eb0d789e97d5f24c9b2274c0996114aa62c1a7fb
SemenovTimofei <st117342@student.spbu.ru>
2023-10-03 23:59:17 +0300

    Rewrite main function to allow for further testing Add testing Change variable names

--- Homework 3/Поиск/main.c
+++ Homework 3/Поиск/main.c
@@ -4,19 +4,19 @@
 
 void swap(int* firstValue, int* secondValue)
 {
-	int temporaryValue = *firstValue;
-	*firstValue = *secondValue;
-	*secondValue = temporaryValue;
+    int temporaryValue = *firstValue;
+    *firstValue = *secondValue;
+    *secondValue = temporaryValue;
 }
 
 void printArray(int array[], int size)
 {
-	for (int i = 0; i < size; i++)
-	{
-		printf("%d ", array[i]);
-	}
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
 
-	printf("\n");
+    printf("\n");
 }
 
 void insertionSort(int array[], int size)
@@ -116,75 +116,117 @@ void quickSort(int array[], int start, int end)
     }
 }
 
-int binarySearch(int array[], int start, int end, int value)
+bool binarySearch(int array[], int start, int end, int key)
 {
+    // sort the main array
+    int quickSortStart = smallNumberPartition(array, end);
+    insertionSort(array, quickSortStart + 1);
+    quickSort(array, quickSortStart, end - 1);
+
+    // binary search for key element
     if (start <= end)
     {
         int middle = start + (end - start) / 2;
 
-        if (array[middle] == value)
+        if (array[middle] == key)
             return true;
 
-        if (array[middle] > value)
-            return binarySearch(array, start, middle - 1, value);
+        if (array[middle] > key)
+            return binarySearch(array, start, middle - 1, key);
 
-        return binarySearch(array, middle + 1, end, value);
+        return binarySearch(array, middle + 1, end, key);
     }
 
     return false;
 }
 
+bool testCorrectCase()
+{
+    int array1[] = { 1, 3, 5, 7, 9 };
+    int searchArray1[] = { 0, 2, 4, 6, 8 };
+    for (int i = 0; i < 5; ++i)
+    {
+        if (binarySearch(array1, 0, 5, searchArray1[i]))
+        {
+            return false;
+        }
+    }
+
+    int array2[] = { -2, -4, -6, -8, -10 };
+    int searchArray2[] = { 0, 2, 4, 6, 8 };
+    for (int i = 0; i < 5; ++i)
+    {
+        if (binarySearch(array2, 0, 5, searchArray2[i]))
+        {
+            return false;
+        }
+    }
+
+    int array3[] = { 1, 2, 3, 4, 5};
+    int searchArray3[] = { 0, 2, 4, 6, 8 };
+    for (int i = 0; i < 5; ++i)
+    {
+        if (!binarySearch(array3, 0, 5, searchArray3[1]) || !binarySearch(array3, 0, 5, searchArray3[2]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void main()
 {
-	printf("Input number of elements in array\n");
-	int n = -1;
-	scanf_s("%d", &n);
+    if (!testCorrectCase())
+    {
+        printf("Test failed\n");
+        return;
+    }
 
-    if (n < 1)
+    printf("Input number of elements in array\n");
+    int mainSize = -1;
+    scanf_s("%d", &mainSize);
+
+    if (mainSize < 1)
     {
         printf("An array must have at least one element\n");
         return;
     }
 
-	printf("Input number of random elements\n");
-    int k = -1;
-    scanf_s("%d", &k);
+    printf("Input number of random elements\n");
+    int secondarySize = -1;
+    scanf_s("%d", &secondarySize);
 
-    if (k < 1)
+    if (secondarySize < 1)
     {
         printf("An array must have at least one element\n");
         return;
     }
 
-    int* array = calloc(n, sizeof(int));
-	srand((unsigned int)time(NULL));
+    int* array = calloc(mainSize, sizeof(int));
+    srand((unsigned int)time(NULL));
 
-	for (int i = 0; i < n; ++i)
-	{
-		array[i] = rand() % 100;
-	}
+    for (int i = 0; i < mainSize; ++i)
+    {
+        array[i] = rand() % 100;
+    }
 
-    int* searchArray = calloc(k, sizeof(int));
+    int* searchArray = calloc(secondarySize, sizeof(int));
 
-	for (int i = 0; i < n; ++i)
-	{
-		searchArray[i] = rand() % 100;
-	}
-
-    // sort the main array
-    int quickSortStart = smallNumberPartition(array, n);
-    insertionSort(array, quickSortStart + 1);
-    quickSort(array, quickSortStart, n - 1);
+    for (int i = 0; i < mainSize; ++i)
+    {
+        searchArray[i] = rand() % 100;
+    }
 
     // sort the secondary array
-    quickSortStart = smallNumberPartition(searchArray, k);
+    int quickSortStart = smallNumberPartition(searchArray, secondarySize);
     insertionSort(searchArray, quickSortStart + 1);
-    quickSort(searchArray, quickSortStart, k - 1);
+    quickSort(searchArray, quickSortStart, secondarySize - 1);
 
     // finding an element in the main array
-    for (int i = 0; i < k; ++i)
+    for (int i = 0; i < secondarySize; ++i)
     {
-        if (binarySearch(array, 0, n, searchArray[i]))
+        if (binarySearch(array, 0, mainSize, searchArray[i]))
         {
             printf("%d is in the main array\n", searchArray[i]);
         }

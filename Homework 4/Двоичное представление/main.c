f15d9a677e876d77742c8671e3530cec92f3c2a0
SemenovTimofei <st117342@student.spbu.ru>
2023-10-08 22:25:04 +0300

    Add testing

--- Homework 4/Двоичное представление/main.c
+++ Homework 4/Двоичное представление/main.c
@@ -5,9 +5,9 @@
 
 char* binaryRepresentation(int number, int length)
 {
-    char* binary = (char*)malloc(sizeof(char) * length);
+    char* binary = (char*)calloc(length, length * sizeof(char));
     int k = 0;
-    for (unsigned i = (1 << length - 1); i > 0; i = i / 2)
+    for (int i = 1 << length - 1; i > 0; i = i / 2)
     {
         binary[k++] = (number & i) ? '1' : '0';
     }
@@ -17,58 +17,144 @@ char* binaryRepresentation(int number, int length)
     return binary;
 }
 
-char* binaryAddition(char number1[], char number2[], int length)
+char* binaryAddition(char firstNumber[], char secondNumber[], int length)
 {
     char* addition = (char*)calloc(length, length * sizeof(char));
     bool carryover = false;
 
     for (int i = length - 1; i >= 0; --i)
     {
-        if ((number1[i] == '1') && (number2[i] == '1'))
+        // случай, когда есть остаток от суммы предыдущих цифр
+        if (carryover)
         {
-            carryover = true;
-            addition[i] = '0';
-        }
-        else if (((number1[i] == '0') && (number2[i] == '1') && carryover) || ((number1[i] == '1') && (number2[i] == '0') && carryover))
-        {
-            carryover = true;
-            addition[i] = '0';
-        }
-        else if (((number1[i] == '0') && (number2[i] == '1') && !carryover) || ((number1[i] == '1') && (number2[i] == '0') && !carryover))
-        {
-            addition[i] = '1';
-        }
-        else if ((number1[i] == '0') && (number2[i] == '0') && carryover)
-        {
-            addition[i] = '1';
-            carryover = false;
-        }
-        else if ((number1[i] == '0') && (number2[i] == '0') && !carryover)
-        {
-            addition[i] = '0';
-            carryover = false;
+            if ((firstNumber[i] == '1') && (secondNumber[i] == '1'))
+            {
+                addition[i] = '1';
+                carryover = true;
+            }
+            else if (((firstNumber[i] == '0') && (secondNumber[i] == '1')) || ((firstNumber[i] == '1') && (secondNumber[i] == '0')))
+            {
+                addition[i] = '0';
+                carryover = true;
+            }
+            else if ((firstNumber[i] == '0') && (secondNumber[i] == '0'))
+            {
+                addition[i] = '1';
+                carryover = false;
+            }
         }
 
-        printf("%c %c %c\n", number1[i], number2[i], addition[i]);
+        // случай, когда нет остатка от суммы предыдущих цифр
+        else
+        {
+            if ((firstNumber[i] == '1') && (secondNumber[i] == '1'))
+            {
+                addition[i] = '0';
+                carryover = true;
+            }
+            else if (((firstNumber[i] == '0') && (secondNumber[i] == '1')) || ((firstNumber[i] == '1') && (secondNumber[i] == '0')))
+            {
+                addition[i] = '1';
+                carryover = false;
+            }
+            else if ((firstNumber[i] == '0') && (secondNumber[i] == '0'))
+            {
+                addition[i] = '0';
+                carryover = false;
+            }
+        }
     }
+
+    return addition;
+}
+
+int binaryToDecimal(char binary[], int length)
+{
+    int decimal = 0;
+    int binaryNumber = 1;
+
+    for (int i = length - 1; i >= 0; --i)
+    {
+        if (binary[i] == '1')
+        {
+            decimal += binaryNumber;
+        }
+
+        binaryNumber *= 2;
+    }
+
+    return decimal;
+}
+
+bool testing()
+{
+    srand(time(NULL));
+
+    int length = 9;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        int firstNumber = rand() % 256;
+        int secondNumber = rand() % 256;
+
+        char* binaryFirstNumber = binaryRepresentation(firstNumber, length);
+        char* binarySecondNumber = binaryRepresentation(secondNumber, length);
+        char* sum = binaryAddition(binaryFirstNumber, binarySecondNumber, length);
+        int decimalSum = binaryToDecimal(sum, length);
+
+        if (!(firstNumber + secondNumber == decimalSum))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void main()
 {
+    if (!testing())
+    {
+        printf("Test failed\n");
+        return;
+    }
+
     setlocale(LC_ALL, "Rus");
 
-    int firstNumber = 10;
-    int secondNumber = -10;
+    printf("Введите первое число\n");
+    int firstNumber = 0;
+    scanf_s("%d", &firstNumber);
 
-    int length = 8;
+    // поддерживает числа от -65536 до 65536 включительно (2^16)
+    if (firstNumber < -65535 || firstNumber > 65535)
+    {
+        printf("Числа меньше -65535 или больше 65535 не поддерживаются\n");
+        return;
+    }
+    
+    printf("Введите второе число\n");
+    int secondNumber = 0;
+    scanf_s("%d", &secondNumber);
+
+    if (secondNumber < -65535 || secondNumber > 65535)
+    {
+        printf("Числа меньше -65535 или больше 65535 не поддерживаются\n");
+        return;
+    }
+
+    int length = 17;
 
     char* binaryFirstNumber = binaryRepresentation(firstNumber, length);
-    printf("The binary representation of %d is %s\n", firstNumber, binaryFirstNumber);
+    printf("Число %d в двоичном представлении это %s\n", firstNumber, binaryFirstNumber);
 
     char* binarySecondNumber = binaryRepresentation(secondNumber, length);
-    printf("The binary representation of %d is %s\n", secondNumber, binarySecondNumber);
+    printf("Число %d в двоичном представлении это %s\n", secondNumber, binarySecondNumber);
 
-    char* addition = binaryAddition(binaryFirstNumber, binarySecondNumber, length);
+    char* sum = binaryAddition(binaryFirstNumber, binarySecondNumber, length);
+    printf("Двоичная сумма чисел равна %s\n", sum);
+
+    int decimalSum = binaryToDecimal(sum, length);
+    printf("Сумма в десятеричном представлении это %d", decimalSum);
 
     return;
 }

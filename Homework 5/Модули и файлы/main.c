b4522147a4570a98174fd0f99cd5809d2406f2da
SemenovTimofei <st117342@student.spbu.ru>
2023-10-12 17:45:52 +0300

    Add Modules and Files

---
+++ Homework 5/Модули и файлы/main.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* file;
+    char* filename = "sample.txt";
+    char* mode = "r";
+
+    if (fopen_s(&file, filename, mode) != 0) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* fileContents = (char*)calloc(fileSize, fileSize * sizeof(char));
+
+    fread(fileContents, sizeof(char), fileSize, file);
+
+    fclose(file);
+
+    // Use the fileContents array as needed
+    
+    int array[11];
+
+    for (int i = 0; fileContents[i] != '\0'; i++)
+    {
+        array[i] = (fileContents[i] - '0');
+    }
+
+    printArray(array, fileSize);
+
+    //mostFrequentElement(fileContents, fileSize);
+
+    free(fileContents);
+
+    return 0;
+}

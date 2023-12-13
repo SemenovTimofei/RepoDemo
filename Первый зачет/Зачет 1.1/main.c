de9b8c0ef65e5916522b735e59ac7db833bb86fc
SemenovTimofei <st117342@student.spbu.ru>
2023-12-13 14:42:59 +0300

    Add testing

--- Первый зачет/Зачет 1.1/main.c
+++ Первый зачет/Зачет 1.1/main.c
@@ -1,13 +1,82 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int smallNumber(int num)
+void smallNumber(char num[], size_t size)
 {
-    int result = 0;
+	for (int i = 0; i < size - 1; ++i)
+	{
+		bool swap = false;
 
+		for (int j = 0; j < size - i - 1; ++j)
+		{
+			if (num[j] > num[j + 1])
+			{
+				char temporaryValue = num[j];
+				num[j] = num[j + 1];
+				num[j + 1] = temporaryValue;
+				swap = true;
+			}
+		}
+
+		if (swap == false)
+		{
+			break;
+		}
+	}
+
+	return;
+}
+
+bool testing()
+{
+	char test1[] = "11111";
+	size_t size1 = strlen(test1);
+	smallNumber(test1, size1);
+	for (size_t i = 0; i < size1 - 1; ++i)
+	{
+		if (test1[i] > test1[i + 1])
+		{
+			return false;
+		}
+	}
+
+	char test2[] = "987654321";
+	size_t size2 = strlen(test2);
+	smallNumber(test2, size2);
+	for (size_t i = 0; i < size2 - 1; ++i)
+	{
+		if (test2[i] > test2[i + 1])
+		{
+			return false;
+		}
+	}
+	
+	char test3[] = "12321";
+	size_t size3 = strlen(test3);
+	smallNumber(test3, size3);
+	for (size_t i = 0; i < size3 - 1; ++i)
+	{
+		if (test3[i] > test3[i + 1])
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
 int main()
 {
-    int num = 321;
-    
+	if (!testing())
+	{
+		printf("Testing failed\n");
+		return 1;
+	}
+
+    char num[] = "987612345";
+    smallNumber(num, strlen(num));
+	printf("The smallest number: %s\n", num);
+
+	return 0;
 }

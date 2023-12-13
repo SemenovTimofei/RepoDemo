08bb0eb56cc010669261245293d0a31c14e2237d
SemenovTimofei <st117342@student.spbu.ru>
2023-12-13 14:02:40 +0300

    Add second task

---
+++ Первый зачет/Зачет 1.2/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+#include "queue.h"
+
+int main()
+{
+	if (!testing())
+	{
+		printf("Testing failed\n");
+		return 1;
+	}
+
+	List* list = NULL;
+
+	int command = -1;
+	int elementCount = 0;
+
+	while (command != 0)
+	{
+		printf("Enter the command: ");
+		scanf_s("%d", &command);
+		switch (command)
+		{
+		case 0:
+		{
+			if (elementCount == 0)
+			{
+				free(list);
+				return 0;
+			}
+			deleteList(&list);
+			return 0;
+		}
+		case 1:
+		{
+			printf("Enter key: ");
+
+			int key = 0;
+			if (scanf_s("%d", &key) == NULL)
+			{
+				printf("Invalid input\n");
+				return 1;
+			}
+
+			printf("Enter value: ");
+
+			int value = 0;
+			if (scanf_s("%d", &value) == NULL)
+			{
+				printf("Invalid input\n");
+				return 1;
+			}
+
+			push(&list, key, value);
+			++elementCount;
+
+			break;
+		}
+		case 2:
+		{
+			printf("Enter index of the element to delete: ");
+			int index = 0;
+			if (scanf_s("%d", &index) == NULL)
+			{
+				printf("Invalid input\n");
+				return 1;
+			}
+
+			if (elementCount == 0)
+			{
+				printf("The list is empty\n");
+				break;
+			}
+
+			if (0 <= index && index <= elementCount - 1)
+			{
+				deleteElement(&list, index);
+				--elementCount;
+				break;
+			}
+
+			printf("Invalid index\n");
+			break;
+		}
+		case 3:
+		{
+			if (elementCount > 0)
+			{
+				printList(list);
+				break;
+			}
+
+			printf("The list is empty\n");
+			break;
+		}
+		default:
+		{
+			printf("Invalid command\n");
+			return 1;
+		}
+		}
+	}
+}
---
+++ Первый зачет/Зачет 1.2/queue.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdbool.h>
+
+// list structure
+typedef struct List List;
+
+// adds an element to the list
+int push(List** head, int key, int value);
+
+// deletes the head element
+int pop(List** head);
+
+// returns a key of an element at a given index
+List* getElement(List* head, size_t index);
+
+// deletes an element at a given index
+int deleteElement(List** head, size_t index);
+
+// deletes the whole list
+void deleteList(List** head);
+
+// prints out all list values
+void printList(const List* head);
+
+// testing list operations
+bool testing();

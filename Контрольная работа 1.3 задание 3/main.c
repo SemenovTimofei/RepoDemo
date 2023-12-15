e276ef55620dcd49f2d6534605f61b88b3e634f4
SemenovTimofei <st117342@student.spbu.ru>
2023-12-15 15:04:03 +0300

    Add testing

--- Контрольная работа 1.3 задание 3/main.c
+++ Контрольная работа 1.3 задание 3/main.c
@@ -7,29 +7,73 @@
 
 void deleteCopies(char string[])
 {
-    char* current = string;
+    char* newString = string;
     char* example = string + 1;
 
     while (*example != NULL)
     {
-        if (*example != *current)
+        if (*example != *newString)
         {
-            ++current;
-            *current = *example;
+            ++newString;
+            *newString = *example;
         }
         ++example;
     }
 
-    *(++current) = '\0';
+    *(++newString) = '\0';
 }
 
 bool testing()
 {
+    FILE* file1 = NULL;
+    fopen_s(&file1, "test1.txt", "r");
+    if (file1 == NULL)
+    {
+        return false;
+    }
 
+    char string1[MAX_LENGTH] = { '\0' };
+    if (fscanf_s(file1, "%s", string1, MAX_LENGTH) != 1)
+    {
+        return false;
+    }
+
+    deleteCopies(string1);
+    if (strcmp(string1, "ababababab") != 0)
+    {
+        return false;
+    }
+
+    FILE* file2 = NULL;
+    fopen_s(&file2, "test2.txt", "r");
+    if (file2 == NULL)
+    {
+        return false;
+    }
+
+    char string2[MAX_LENGTH] = { '\0' };
+    if (fscanf_s(file2, "%s", string2, MAX_LENGTH) != 1)
+    {
+        return false;
+    }
+
+    deleteCopies(string2);
+    if (strcmp(string2, "abababa\nqwewrty") != 0)
+    {
+        return false;
+        printf("f %s f", string2);
+    }
+
+    return true;
 }
 
 int main()
 {
+    if (!testing())
+    {
+        printf("Testing failed\n");
+        return 1;
+    }
     FILE* file = NULL;
     fopen_s(&file, "text.txt", "r");
     if (file == NULL)
@@ -46,7 +90,6 @@ int main()
 
     deleteCopies(string);
     printf("The edited string: %s\n", string);
-}
 
-// aababaasddsaaabsaasss
-// ababasdsabsas
+    return 0;
+}

8b415d7c8c3a08d14dc8cd2c87020a122adfd245
SemenovTimofei <st117342@student.spbu.ru>
2023-12-22 14:31:48 +0300

    Finish transitiveClosure function

--- Контрольная работа 2 переписывание 2/Контрольная работа 2.2 задача 2/main.c
+++ Контрольная работа 2 переписывание 2/Контрольная работа 2.2 задача 2/main.c
@@ -2,102 +2,86 @@
 
 #define MAX_SIZE 100
 
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void writeToFile(int matrix[MAX_SIZE][MAX_SIZE], size_t n) // change to FILE* file later
 {
-	FILE* output = fopen("output.txt", "w");
-	for (size_t i = 0; i < n; ++i)
-	{
-		for (size_t j = 0; j < n; ++j)
-		{
-			fprintf(output, "%d ", matrix[i][j]);
-		}
-		fprintf(output, "\n");
-	}
-	fclose(output);
-	return;
+    FILE* output = fopen("output.txt", "w");
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            fprintf(output, "%d ", matrix[i][j]);
+        }
+        fprintf(output, "\n");
+    }
+
+    printMatrix(matrix, n);
+    fclose(output);
+    return;
 }
 
 void transitiveClosure(int matrix[MAX_SIZE][MAX_SIZE], size_t n)
 {
-	int newMatrix[MAX_SIZE][MAX_SIZE];
-	for (size_t i = 0; i < n; ++i)
-	{
-		for (size_t j = 0; j < n; ++j)
-		{
-			newMatrix[i][j] = matrix[i][j];
-		}
-	}
+    int newMatrix[MAX_SIZE][MAX_SIZE];
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            newMatrix[i][j] = matrix[i][j];
+        }
+    }
 
-	for (size_t k = 0; k < n; ++k)
-	{
-		for (size_t i = 0; i < n; ++i)
-		{
-			for (size_t j = 0; j < n; ++j)
-			{
-				if (newMatrix[i][k] == 1 && newMatrix[k][j] == 1)
-				{
-					newMatrix[i][j] = 1;
-				}
-			}
-		}
-	}
-	
-	writeToFile(newMatrix, n);
-	return;
-}
-
-void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], size_t n)
-{
-	for (size_t i = 0; i < n; ++i)
-	{
-		for (size_t j = 0; j < n; ++j)
-		{
-			printf("%d ", matrix[i][j]);
-		}
-		printf("\n");
-	}
+    for (size_t k = 0; k < n; ++k)
+    {
+        for (size_t i = 0; i < n; ++i)
+        {
+            for (size_t j = 0; j < n; ++j)
+            {
+                newMatrix[i][j] = newMatrix[i][j] || (newMatrix[i][k] && newMatrix[k][j]);
+            }
+        }
+    }
+    
+    writeToFile(newMatrix, n);
+    return;
 }
 
 int main()
 {
-	/*
-	int matrix[n][n] = { { 0, 1, 1, 1 }, { 1, 0, 1, 0 }, { 1, 1, 0, 1 }, { 1, 0, 1, 0} };
+    size_t n = 0;
+    FILE* input = NULL;
+    fopen_s(&input, "input.txt", "r");
+    if (input == NULL)
+    {
+        printf("Error openning file\n");
+        return 1;
+    }
+    fscanf_s(input, "%d", &n);
 
-	for (size_t k = 0; k < n; ++k)
-	{
-		for (size_t i = 0; i < n; ++k)
-		{
-			for (size_t j = 0; j < n; ++j)
-			{
-				if (matrix[])
-			}
-		}
-	}
-	*/
+    int matrix[MAX_SIZE][MAX_SIZE];
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            fscanf_s(input, "%d", &matrix[i][j]);
+        }
+    }
 
-	size_t n = 0;
-	FILE* input = NULL;
-	fopen_s(&input, "input.txt", "r");
-	if (input == NULL)
-	{
-		printf("Error openning file\n");
-		return 1;
-	}
-	fscanf_s(input, "%d", &n);
+    printMatrix(matrix, n);
+    printf("\n");
+    transitiveClosure(matrix, n);
 
-	int matrix[MAX_SIZE][MAX_SIZE];
-	for (size_t i = 0; i < n; ++i)
-	{
-		for (size_t j = 0; j < n; ++j)
-		{
-			fscanf_s(input, "%d", &matrix[i][j]);
-		}
-	}
-
-	printMatrix(matrix, n);
-	printf("\n");
-	transitiveClosure(matrix, n);
-	printMatrix(matrix, n);
-	fclose(input);
-	return 0;
+    fclose(input);
+    return 0;
 }

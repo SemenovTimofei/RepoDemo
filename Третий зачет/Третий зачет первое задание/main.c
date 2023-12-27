fca8881a1cb1eeaab8fe065cf71a9b37e03fe8df
SemenovTimofei <st117342@student.spbu.ru>
2023-12-27 12:08:09 +0300

    Move functions to modules

--- Третий зачет/Третий зачет первое задание/main.c
+++ Третий зачет/Третий зачет первое задание/main.c
@@ -1,42 +1,6 @@
 #include <stdio.h>
 
-#define MAX_SIZE 5
-
-void swapColumns(int martix[][MAX_SIZE], size_t firstColumn, size_t secondColumn, size_t rowLength)
-{
-    for (int i = 0; i < rowLength; ++i)
-    {
-        int temporary = martix[i][firstColumn];
-        martix[i][firstColumn] = martix[i][secondColumn];
-        martix[i][secondColumn] = temporary;
-    }
-}
-
-void sortMatrix(int martix[][MAX_SIZE], size_t columnLength, size_t rowLength)
-{
-    for (int i = 0; i < columnLength - 1; ++i)
-    {
-        for (int j = 0; j < columnLength - i - 1; ++j)
-        {
-            if (martix[0][j] > martix[0][j + 1])
-            {
-                swapColumns(martix, j, j + 1, rowLength);
-            }
-        }
-    }
-}
-
-void printMatrix(int martix[][MAX_SIZE], size_t columnLength, size_t rowLength)
-{
-    for (size_t i = 0; i < rowLength; ++i)
-    {
-        for (size_t j = 0; j < columnLength; ++j)
-        {
-            printf("%d ", martix[i][j]);
-        }
-        printf("\n");
-    }
-}
+#include "matrixSort.h"
 
 int main()
 {
@@ -47,7 +11,7 @@ int main()
         { 4, 1, 5, 3, 2 } };
     size_t columnLength = sizeof(matrix[0]) / sizeof(matrix[0][0]);
     size_t rowLength = sizeof(matrix) / sizeof(matrix[0]);
-    printf("Original matrix: \n", columnLength, rowLength);
+    printf("Original matrix: \n");
     printMatrix(matrix, columnLength, rowLength);
 
     sortMatrix(matrix, columnLength, rowLength);
---
+++ Третий зачет/Третий зачет первое задание/matrixSort.c
@@ -0,0 +1,37 @@
+#include "matrixSort.h"
+
+void swapColumns(int martix[][MAX_SIZE], size_t firstColumn, size_t secondColumn, size_t rowLength)
+{
+    for (int i = 0; i < rowLength; ++i)
+    {
+        int temporary = martix[i][firstColumn];
+        martix[i][firstColumn] = martix[i][secondColumn];
+        martix[i][secondColumn] = temporary;
+    }
+}
+
+void sortMatrix(int martix[][MAX_SIZE], size_t columnLength, size_t rowLength)
+{
+    for (int i = 0; i < columnLength - 1; ++i)
+    {
+        for (int j = 0; j < columnLength - i - 1; ++j)
+        {
+            if (martix[0][j] > martix[0][j + 1])
+            {
+                swapColumns(martix, j, j + 1, rowLength);
+            }
+        }
+    }
+}
+
+void printMatrix(int martix[][MAX_SIZE], size_t columnLength, size_t rowLength)
+{
+    for (size_t i = 0; i < rowLength; ++i)
+    {
+        for (size_t j = 0; j < columnLength; ++j)
+        {
+            printf("%d ", martix[i][j]);
+        }
+        printf("\n");
+    }
+}
